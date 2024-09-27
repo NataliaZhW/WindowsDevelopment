9fe5004bfd9380346260c390a1e9e198ca5381b9
user <user@WIN-S3FLAGBG853>
2024-09-27 22:01:07 +0300

    Dialog

--- WindowsDevelopment/main.cpp
+++ WindowsDevelopment/main.cpp
@@ -1,13 +1,44 @@
 #include<Windows.h>
+#include"resource.h"
+
+
+BOOL CALLBACK DlgProg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-
-
-
+	//MessageBox(NULL, "Hello Windows!\nThis is MessageBox ", "Window title",  MB_ICONWARNING  |  MB_HELP);	//MB_ICONERROR |MB_DEFBUTTON2|MB_ICONQUESTION MB_ICONINFORMATION MB_OK MB_ABORTRETRYIGNORE
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, (DLGPROC)DlgProg, 0);
 	return 0;
 }
-
-
-
-
+BOOL CALLBACK DlgProg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON_COPY:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		break;
+		case IDOK: MessageBox(hwnd, "¡€À¿ Õ¿∆¿“¿  ÕŒœ ¿ Œ ", "Info", MB_OK | MB_ICONINFORMATION);
+			break;
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			break;
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
+}
