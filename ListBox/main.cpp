f81ea6205bf050a8b8afc2a461e5c8b1b8aee9ea
user <user@WIN-S3FLAGBG853>
2024-10-07 18:34:57 +0300

    LB_FINDSTRING

--- ListBox/main.cpp
+++ ListBox/main.cpp
@@ -16,7 +16,6 @@ BOOL CALLBACK DlgProg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-//std::list<string> list = { "1This", "is", "my", "first", "Combo","Box" };
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, (DLGPROC)DlgProg, 0);
 	return 0;
 }
@@ -24,8 +23,8 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 BOOL CALLBACK DlgProg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
-	{	
-	case WM_INITDIALOG:		
+	{
+	case WM_INITDIALOG:
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
@@ -36,6 +35,7 @@ BOOL CALLBACK DlgProg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			SendMessage(hCombo, LB_ADDSTRING, 0, (LPARAM)g_LIST_BOX_ITEM[i]);
 		}
 		SendMessage(hCombo, CB_SETCURSEL, 0, 0);
+		//SendMessage(hCombo, CB_SORT, 0, 0);//LBS_SORT, LB_INSERTSTRING , LB_RESETCONTENT очистка, LB_SELECTSTRING поиск, LB_SETLOCALE, 
 		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_ADD);
 		SendMessage(hEditLogin, WM_SETTEXT, 0, (__int64)g_sz_INVITATION);
 	}
@@ -53,11 +53,31 @@ BOOL CALLBACK DlgProg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			HWND hEditAdd = GetDlgItem(hwnd, IDC_EDIT_ADD);//Функция GetDlgItem() по ID-ресурса дочернего окна возвращает HWND соответствующего дочернего окна.
 			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
 			//3) Читаем текст :
-			SendMessage(hEditAdd, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);			
+			SendMessage(hEditAdd, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
 			//4) Загружаем содржимое текстового буфера в поле :
-			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)sz_buffer);			
+			//SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+
+
+			/*int x = SendMessage(hList, LB_FINDSTRING, -1, (LPARAM)sz_buffer);
+			CHAR sz_message[SIZE]{}; 
+			sprintf_s(sz_message, "элемент № %i,", x+1 );
+			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);*/
+
+			if (SendMessage(hList, LB_FINDSTRING, -1, (LPARAM)sz_buffer) != CB_ERR)
+			{
+				MessageBox(hwnd, "Уже есть", "Info", MB_OK | MB_ICONINFORMATION);
+			}
+			else
+			{				
+			if (strcmp(sz_buffer, g_sz_INVITATION) == 0)
+				{
+					MessageBox(hwnd, "Пустое значение", "Info", MB_OK | MB_ICONINFORMATION); 
+					break;
+				}
+				SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+			}
 		}
-		break; 
+		break;
 		case IDC_EDIT_ADD:
 		{
 			CONST INT SIZE = 256; //Длина соообщения
@@ -74,16 +94,16 @@ BOOL CALLBACK DlgProg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			if (HIWORD(wParam) == EN_KILLFOCUS && strcmp(sz_buffer, "") == 0)
 				// Если окно теряет фокус (HIWORD(wParam) поменялся на EN_KILLFOCUS) && "значение" sz_buffer было пустым "", то устанавливаем строку "предопределенное" 
 				SendMessage(hEditAdd, WM_SETTEXT, 0, (LPARAM)g_sz_INVITATION);
-		}		
+		}
 		break;
-		
+
 		case IDC_BUTTON_DEL:
 		{
 			CONST INT SIZE = 256;
 			CHAR sz_buffer[SIZE] = {};
 			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
 			int i = SendMessage(hList, LB_GETCURSEL, 0, 0);
-			SendMessage(hList, LB_DELETESTRING, i, 0);	
+			SendMessage(hList, LB_DELETESTRING, i, 0);
 		}
 		break;
 		case IDOK:
@@ -94,10 +114,17 @@ BOOL CALLBACK DlgProg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			int i = SendMessage(hCombo, LB_GETCURSEL, 0, 0);
 			SendMessage(hCombo, LB_GETTEXT, i, (LPARAM)sz_buffer);
 			CHAR sz_message[SIZE]{};
-			sprintf_s(sz_message, "Вы выбрали элемент № %i, со значением \"%s\"", i, (LPARAM)sz_buffer);
+			if (i != LB_ERR)
+			{
+				sprintf_s(sz_message, "Вы выбрали элемент № %i, со значением \"%s\"", i, (LPARAM)sz_buffer);
+			}
+			else 
+			{ 
+				sprintf_s(sz_message, "Вы не выбрали элемент"); 
+			}
 			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
 		}
-		break;		
+		break;
 
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 		}
